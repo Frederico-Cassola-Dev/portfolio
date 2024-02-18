c3e33edebe734c50292c0dd7552ddb7f1e5fef9c
Your Name <your@email.com>
2024-02-18 12:35:31 +0100

    Add dependencies and some folder structure

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="navBar-container">
+      <ul className="list-container">
+        <li className="list-item">Home</li>
+        <li className="list-item">Projects</li>
+        <li className="list-item">About</li>
+        <li className="list-item">Contact</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <p>
+        Hello, I’m Frederico Cassola a <span>Full-Stack Web Developer</span> who
+        turns ideas into reality.
+      </p>
+    </div>
+  );
+}
