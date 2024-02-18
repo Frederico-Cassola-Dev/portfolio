4c21c654d3e5c8b625c08e52efc7caf9babe7eda
Your Name <your@email.com>
2024-02-18 12:38:47 +0100

    Add dependencies and some folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home/Home";
 import "./App.css";
 
@@ -7,10 +6,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
-  
   );
 }
 
